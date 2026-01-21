6f3509f4852deaadb23412d6b336fe104c952b23
truongtq03 <quangtruong11092k3@gmail.com>
2026-01-21 22:30:30 +0700

    Add empty project structure

---
+++ MyAssignment/MainActivity.java
@@ -0,0 +1,6 @@
+public class MainActivity {
+    public static void main(String[] args) {
+        // Project trống khởi tạo cho Step 2
+        System.out.println("Hello Android Project");
+    }
+}
