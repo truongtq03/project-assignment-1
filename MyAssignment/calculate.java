31e6777c5704a8aabc46d7df9ed047f2c9e5a2af
truongtq03 <quangtruong11092k3@gmail.com>
2026-01-21 23:45:15 +0700

    Finish implement for this branch

--- MyAssignment/calculate.java
+++ MyAssignment/calculate.java
@@ -1,8 +1,10 @@
 public class calculate {
     private Double calculate(SIGN sign, Double arg1, Double arg2) {
         switch (sign) {
-            case MULTIPLY:
-                return arg1 * arg2;
+            case DIVIDE:
+                if (arg2 == 0)
+                    return 0d;
+                return arg1 / arg2;
             default:
                 break;
         }
